db7d05c938a9d6ee9cf55b5c0b67dd28328c6e43
jerryshao <saisai.shao@intel.com>
2014-06-09 16:50:16 +0800

    initial works of move combiner to ShuffleManager's reader and writer

--- core/src/main/scala/org/apache/spark/scheduler/ShuffleMapTask.scala
+++ core/src/main/scala/org/apache/spark/scheduler/ShuffleMapTask.scala
@@ -147,9 +147,7 @@ private[spark] class ShuffleMapTask(
     try {
       val manager = SparkEnv.get.shuffleManager
       writer = manager.getWriter[Any, Any](dep.shuffleHandle, partitionId, context)
-      for (elem <- rdd.iterator(split, context)) {
-        writer.write(elem.asInstanceOf[Product2[Any, Any]])
-      }
+      writer.write(rdd.iterator(split, context).asInstanceOf[Iterator[_ <: Product2[Any, Any]]])
       return writer.stop(success = true).get
     } catch {
       case e: Exception =>
